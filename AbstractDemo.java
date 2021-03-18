9aabada687319faadcc94e13feacd4d36c862c82
JenniferLayug <79358441+JenniferLayug@users.noreply.github.com>
2021-03-18 12:30:57 +0800

    Create AbstractDemo.java

---
+++ AbstractDemo.java
@@ -0,0 +1,13 @@
+public class AbstractDemo {
+
+  
+    public static void main(String[] args) {
+       Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3,3600.00);
+       Employee e = new Salary("John Adams", "Boston, MA", 2,2400.00);
+        System.out.println("Call mailCheck using salary reference --");
+         s.mailCheck();
+      System.out.println("\n Call mailCheck using employee reference --");
+      e.mailCheck();
+    }
+    
+}
